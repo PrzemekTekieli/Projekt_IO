6c1fa89404756961134c45ec9d3544689da692d3
wojteklupin <wojteklupin@gmail.com>
2018-11-27 19:37:28 +0100

    Add files via upload

--- gra/script.js
+++ gra/script.js
@@ -1,6 +1,10 @@
+var socket;
+
 function handle(e) {
     if (e.keyCode === 13) { // jeżeli enter
-        document.getElementById('t1').value = document.getElementById('t1').value + '>' + document.getElementById('t2').value + '\n';
+        e.preventDefault(); // zeby nie dodawalo entera
+        document.getElementById('t1').value += '>' + document.getElementById('t2').value + '\n';
+        socket.send(document.getElementById('t2').value);
         document.getElementById('t2').value = '';
         document.getElementById('t1').scrollTop =  document.getElementById('t1').scrollHeight; // przewiń
         return false; // żeby formularz nie został wysłany
@@ -8,4 +12,12 @@ function handle(e) {
     else {
         return false;
     }
+}
+
+function load() {
+    document.getElementById('t1').value='';
+    socket = new WebSocket('ws://localhost:8080');
+    socket.addEventListener('message', function(event) {
+        document.getElementById('t1').value += event.data + '\n';
+    }); 
 }
