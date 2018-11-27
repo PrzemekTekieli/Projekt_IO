95be9ec4139b52f1a09c6c43a17eae07d1aeaf0e
wojteklupin <wojteklupin@gmail.com>
2018-11-27 18:17:22 +0100

    Add files via upload

---
+++ gra/script.js
@@ -0,0 +1,15 @@
+function handle(e) {
+    if (e.keyCode === 13) { // jeżeli enter
+        document.getElementById('t1').value = document.getElementById('t1').value + '>' + document.getElementById('t2').value + '\n';
+        document.getElementById('t2').value = '';
+        document.getElementById('t1').scrollTop =  document.getElementById('t1').scrollHeight; // przewiń
+        return false; // żeby formularz nie został wysłany
+    }
+    else {
+        return false;
+    }
+}
+
+function load() {
+    
+}
